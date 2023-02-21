41fd418ce2a8aa5522276b0e1257dca09a9b378d
Akash Bhadauria <akashbhadauria196@gmail.com>
2023-02-21 15:09:42 +0530

    Updating main

--- main.py
+++ main.py
@@ -1,120 +1,237 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
 from tqdm import tqdm
-
-class Trainer:
-
-    def __init__(self, model, device, criterion, optimizer, batch_size, scheduler=None):
-        self.model = model
-        self.device = device
-        self.criterion = criterion
-        self.optimizer = optimizer
-        self.batch_size = batch_size
-        self.scheduler = scheduler
-        self.train_losses = []
-        self.train_acc = []
-        self.test_losses = []
-        self.test_acc = []
-        self.train_loss_avg = []
-        self.train_acc_avg = []
-        self.misclassified_images = {}
+import torch
+import torch.optim as optim
+from torch.optim.lr_scheduler import StepLR
+import torch.nn as nn
+import numpy as np
 
 
-    def get_train_stats(self):
-        return list(map(lambda x: x.cpu().item(), self.train_loss_avg)), self.train_acc_avg
-    
-    def get_test_stats(self):
-        return list(map(lambda x: x.cpu().item(), self.test_losses)), self.test_acc
-    
-    def train(self, train_loader):
-        self.model.train()
-        pbar = tqdm(train_loader)
-        correct = 0
-        processed = 0
-        loss_epoch = 0
-        acc_epoch = 0 
 
-        for batch_idx, (data, target) in enumerate(pbar):
+def trainOneCLR(model, device, train_loader, criterion, scheduler, optimizer, use_l1=False, lambda_l1=0.01):
 
-            data, target = data.to(self.device), target.to(self.device)
+    model.train()
+    pbar = tqdm(train_loader)
+    lr_trend = []
+    correct = 0
+    processed = 0
+    train_loss = 0
 
-            self.optimizer.zero_grad()
+    for batch_idx, (data, target) in enumerate(pbar):
+        # get samples
+        data, target = data.to(device), target.to(device)
 
-            y_pred = self.model(data)
+        # Init
+        optimizer.zero_grad()
+        # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch 
+        # accumulates the gradients on subsequent backward passes. Because of this, when you start your training loop, 
+        # ideally you should zero out the gradients so that you do the parameter update correctly.
 
-            loss = self.criterion(y_pred, target)
-            self.train_losses.append(loss)
-            loss_epoch += loss
+        # Predict
+        y_pred = model(data)
+        # Calculate loss
+        loss = criterion(y_pred, target)
 
-            loss.backward()
-            self.optimizer.step()
+        l1=0
+        if use_l1:
+            for p in model.parameters():
+                l1 = l1 + p.abs().sum()
+        loss = loss + lambda_l1*l1
+
+        # Backpropagation
+        loss.backward()
+        optimizer.step()
+        # updating LR
+        if scheduler:
+            if not isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step()
+                lr_trend.append(scheduler.get_last_lr()[0])
+
+        train_loss += loss.item()
+
+        # Update pbar-tqdm
+        pred = y_pred.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
+        correct += pred.eq(target.view_as(pred)).sum().item()
+        processed += len(data)
+        
+
+        pbar.set_description(desc= f'Batch_id={batch_idx} Loss={train_loss/(batch_idx + 1):.5f} Accuracy={100*correct/processed:0.2f}%')
+    return 100*correct/processed, train_loss/(batch_idx + 1), lr_trend
 
 
-            pred = y_pred.argmax(dim=1, keepdim=True)
+def testOneCLR(model, device, test_loader, criterion):
+
+    model.eval()
+    test_loss = 0
+    correct = 0
+    with torch.no_grad():
+        for data, target in test_loader:
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+            test_loss += criterion(output, target).item()  # sum up batch loss
+            pred = output.argmax(dim=1, keepdim=True)  # get the index of the max log-probability
             correct += pred.eq(target.view_as(pred)).sum().item()
-            processed += len(data)
-            pbar.set_description(desc= f' Loss={loss.item()} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
-            self.train_acc.append(100*correct/processed)
-        
-        loss_epoch /= len(train_loader.dataset)
-        self.train_loss_avg.append(loss_epoch)
-        self.train_acc_avg.append(100. * correct / len(train_loader.dataset))
-        
-        
 
-    def test(self, test_loader):
-        self.model.eval()
+    test_loss /= len(test_loader)
+
+    print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
+        test_loss, correct, len(test_loader.dataset),
+        100. * correct / len(test_loader.dataset)))
+    return 100. * correct / len(test_loader.dataset), test_loss
+
+
+def save_model(model, epoch, optimizer, path):
+    state = {
+        'epoch': epoch,
+        'state_dict': model.state_dict(),
+        'optimizer': optimizer.state_dict()
+    }
+    torch.save(state, path)
+
+
+def fit_model(net, optimizer, criterion, device, NUM_EPOCHS,train_loader, test_loader, use_l1=False, scheduler=None, save_best=False):
+
+    training_acc, training_loss, testing_acc, testing_loss = list(), list(), list(), list()
+    lr_trend = []
+    if save_best:
+        min_val_loss = np.inf
+        save_path = 'model.pt'
+
+    for epoch in range(1,NUM_EPOCHS+1):
+        print("EPOCH: {} (LR: {})".format(epoch, optimizer.param_groups[0]['lr']))
+        
+        train_acc, train_loss, lr_hist = trainOneCLR(
+            model=net, 
+            device=device, 
+            train_loader=train_loader, 
+            criterion=criterion ,
+            optimizer=optimizer, 
+            use_l1=use_l1, 
+            scheduler=scheduler
+        )
+        test_acc, test_loss = testOneCLR(net, device, test_loader, criterion)
+        # update LR
+        if scheduler:
+            if isinstance(scheduler, torch.optim.lr_scheduler.ReduceLROnPlateau):
+                scheduler.step(test_loss)
+        
+        if save_best:
+            if test_loss < min_val_loss:
+                print(f'Valid loss reduced from {min_val_loss:.5f} to {test_loss:.6f}. checkpoint created at...{save_path}\n')
+                save_model(net, epoch, optimizer, save_path)
+                min_val_loss = test_loss
+            else:
+                print(f'Valid loss did not inprove from {min_val_loss:.5f}\n')
+        else:
+            print()
+
+        training_acc.append(train_acc)
+        training_loss.append(train_loss)
+        testing_acc.append(test_acc)
+        testing_loss.append(test_loss)
+        lr_trend.extend(lr_hist)    
+
+    if scheduler:   
+        return net, (training_acc, training_loss, testing_acc, testing_loss, lr_trend)
+    else:
+        return net, (training_acc, training_loss, testing_acc, testing_loss)
+
+class train:
+    def __init__(self):
+
+        self.train_losses = []
+        self.train_acc    = []
+
+    # Training
+    def execute(self,net, device, trainloader, optimizer, criterion,epoch):
+
+        #print('Epoch: %d' % epoch)
+        net.train()
+        train_loss = 0
+        correct = 0
+        #total = 0
+        processed = 0
+        pbar = tqdm(trainloader)
+
+        for batch_idx, (inputs, targets) in enumerate(pbar):
+            # get samples
+            inputs, targets = inputs.to(device), targets.to(device)
+
+            # Init
+            optimizer.zero_grad()
+
+            # In PyTorch, we need to set the gradients to zero before starting to do backpropragation because PyTorch accumulates the gradients on subsequent backward passes. 
+            # Because of this, when you start your training loop, ideally you should zero out the gradients so that you do the parameter update correctly.
+
+            # Predict
+            outputs = net(inputs)
+
+            # Calculate loss
+            loss = criterion(outputs, targets)
+
+            # Backpropagation
+            loss.backward()
+            optimizer.step()
+
+            train_loss += loss.item()
+            self.train_losses.append(loss.item())
+            
+            _, predicted = outputs.max(1)
+            processed += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            pbar.set_description(desc= f'Epoch: {epoch},Loss={loss.item():3.2f} Batch_id={batch_idx} Accuracy={100*correct/processed:0.2f}')
+            self.train_acc.append(100*correct/processed)
+
+
+class test:
+
+    def __init__(self):
+
+        self.test_losses = []
+        self.test_acc    = []
+
+    def execute(self, net, device, testloader, criterion):
+
+        net.eval()
         test_loss = 0
         correct = 0
-        idx = 0
+        total = 0
         with torch.no_grad():
-            for data, target in test_loader:
-                data, target = data.to(self.device), target.to(self.device)
-                output = self.model(data)
-                test_loss += self.criterion(output, target)
-                pred = output.argmax(dim=1, keepdim=True)
-                correct += pred.eq(target.view_as(pred)).sum().item()
+            for batch_idx, (inputs, targets) in enumerate(testloader):
+                inputs, targets = inputs.to(device), targets.to(device)
+                outputs = net(inputs)
+                loss = criterion(outputs, targets)
 
-        test_loss /= len(test_loader.dataset)
+                test_loss += loss.item()
+                _, predicted = outputs.max(1)
+                total += targets.size(0)
+                correct += predicted.eq(targets).sum().item()
+
+        test_loss /= len(testloader.dataset)
         self.test_losses.append(test_loss)
 
-        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
-            test_loss, correct, len(test_loader.dataset),
-            100. * correct / len(test_loader.dataset)))
-        
-        self.test_acc.append(100. * correct / len(test_loader.dataset))
+        print('Test set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)\n'.format(
+            test_loss, correct, len(testloader.dataset),
+            100. * correct / len(testloader.dataset)))
 
-    
-    def get_misclassified_images(self, test_loader):
-        self.model.eval()
-        idx = 0
-        with torch.no_grad():
-            for data, target in test_loader:
-                data, target = data.to(self.device), target.to(self.device)
-                output  = self.model(data)
-                pred = output.argmax(dim=1, keepdim=True)
+        # Save.
+        self.test_acc.append(100. * correct / len(testloader.dataset))
 
-                for sample in range(data.shape[0]):
-                    if (target[sample] != pred[sample]):
-                        self.misclassified_images[idx] = [data[sample].cpu(), target[sample].cpu(), pred[sample].cpu()]
-                    idx += 1
-        return self.misclassified_images
+def trainNetwork(net, device, trainloader, testloader, EPOCHS, lr=0.2):
 
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr, momentum=0.9)
+    scheduler = StepLR(optimizer, step_size=6, gamma=0.1)
 
-def get_criterion_for_classification():
-    return nn.CrossEntropyLoss()
+    trainObj = train()
+    testObj = test()
 
-def get_sgd_optimizer(model, lr=0.001, momentum=0.9, scheduler=False):
-    optimizer = optim.SGD(model.parameters(),
-                     lr=lr,
-                     momentum=momentum)
-    if scheduler:
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=4,
-                                                    gamma=0.1)
-        return optimizer, scheduler
-    else:
-        return optimizer
-    
-def get_adam_optimizer(model, lr=0.001):
-    return optim.Adam(model.parameters(), lr=lr)
+    for epoch in range(EPOCHS):  # loop over the dataset multiple times
+
+        trainObj.execute(net, device, trainloader, optimizer, criterion, epoch)
+        testObj.execute(net, device, testloader, criterion)
+        scheduler.step()
+
+    print('Finished Training')
+
+    return trainObj, testObj
